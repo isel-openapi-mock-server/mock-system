21f9d7a7047f23f4b449fc7eff21fb0e13b130fd
Diogo Rosa <a50533@alunos.isel.pt>
2025-03-27 17:39:59 +0000

    headers

--- jvm/src/main/kotlin/isel/openapi/mock/http/DynamicHandlers.kt
+++ jvm/src/main/kotlin/isel/openapi/mock/http/DynamicHandlers.kt
@@ -9,51 +9,79 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import io.swagger.v3.oas.models.media.IntegerSchema
 import io.swagger.v3.oas.models.media.ObjectSchema
 import io.swagger.v3.oas.models.media.StringSchema
+import isel.openapi.mock.parsingServices.model.Location
 import isel.openapi.mock.parsingServices.model.Type
+import isel.openapi.mock.utils.Either
+import isel.openapi.mock.utils.Failure
+import isel.openapi.mock.utils.Success
+import isel.openapi.mock.utils.failure
+import isel.openapi.mock.utils.success
+import org.springframework.http.ResponseEntity
 
-interface DynamicHandler {
-
+sealed class VerifyBodyError {
+    data object InvalidBodyTypes: VerifyBodyError()
+    data object InvalidBodyKeys: VerifyBodyError()
+    data object InvalidBodyFormat: VerifyBodyError()
 }
+
+typealias VerifyBodyResult = Either<VerifyBodyError, Boolean>
+
+sealed class VerifyHeadersError {
+    data object InvalidType: VerifyHeadersError()
+    data object InvalidHeader: VerifyHeadersError()
+    data object InvalidContentType: VerifyHeadersError()
+    data object InvalidHeaderFormat: VerifyHeadersError()
+    data object MissingHeader: VerifyHeadersError()
+}
+
+typealias VerifyHeadersResult = Either<VerifyHeadersError, Boolean>
 
 class BodyAndParamsDynamicHandler(
     private val response: String,
     private val params: List<ApiParameter>?,
     private val body: ApiRequestBody?
-): DynamicHandler {
+) {
     @ResponseBody
     fun handle(
         request: HttpServletRequest,
-    ): String {
+    ): ResponseEntity<*> {
         val requestBody = request.reader.readText()
         val requestParams = request.parameterMap.mapValues { it.value[0] }
+        val headers = request.headerNames.toList().associate { it to request.getHeader(it) }
 
-        TODO()
-
-        /*
-        if(params != null) {
-            if(requestParams.keys.any { key -> params.none { it.name == key } }) {
-                return "Response: $response | Params: $requestParams | Error: Invalid parameters"
-            }
-            if(params.filter { it.required }.any { it.name !in requestParams.keys }) {
-                return "Response: $response | Params: $requestParams | Error: Missing required parameters"
-            }
-        }
-
-        if (body != null) {
-            val bodyMap = if(!requestBody.isEmpty()) convertJsonToMap(requestBody) else emptyMap()
-            if(bodyMap.keys != body.parameters.keys) {
-                return "Response: $response | Body: $requestBody | Error: Invalid body"
+        if(body != null) {
+            val bodyResult = verifyBody(requestBody, body)
+            if(bodyResult is Failure) {
+                return when(bodyResult.value) {
+                    is VerifyBodyError.InvalidBodyTypes -> ResponseEntity.badRequest().body("Invalid body types")
+                    is VerifyBodyError.InvalidBodyKeys -> ResponseEntity.badRequest().body("Invalid body keys")
+                    is VerifyBodyError.InvalidBodyFormat -> ResponseEntity.badRequest().body("Invalid body format")
+                }
             }
         }
 
-         */
+        val headersResult = verifyHeaders(headers, params?.filter { it.location == Location.HEADER } ?: emptyList(), body?.contentType ?: "")
+        if(headersResult is Failure) {
+            return when(headersResult.value) {
+                is VerifyHeadersError.InvalidType -> ResponseEntity.badRequest().body("Invalid header type")
+                is VerifyHeadersError.InvalidHeader -> ResponseEntity.badRequest().body("Invalid header")
+                is VerifyHeadersError.InvalidContentType -> ResponseEntity.badRequest().body("Invalid content type")
+                is VerifyHeadersError.InvalidHeaderFormat -> ResponseEntity.badRequest().body("Invalid header format")
+                is VerifyHeadersError.MissingHeader -> ResponseEntity.badRequest().body("Missing header")
+            }
+        }
 
-        return "Response: $response | Body: $requestBody | Params: $requestParams"
+        return ResponseEntity.ok(response)
     }
 
     private fun convertJsonToMap(jsonString: String): Map<String, Any> {
         val objectMapper = jacksonObjectMapper()
-        return objectMapper.readValue(jsonString)
+        return objectMapper.readValue<Map<String, Any>>(jsonString)
+    }
+
+    private fun convertStringToArray(arrayString: String): List<Any> {
+        val objectMapper = jacksonObjectMapper()
+        return objectMapper.readValue<List<Any>>(arrayString)
     }
 
     fun getPathParams() {
@@ -64,43 +92,82 @@ class BodyAndParamsDynamicHandler(
         TODO()
     }
 
-    fun verifyBody(body: String, expectedBody: ApiRequestBody): Boolean {
-
-        if(expectedBody.schemaType is Type.ObjectType) {
-            try {
-                //Converte o body recebido para um map<string,any>
-                val requestBodyMap = convertJsonToMap(body)
-
-                //Body experado extraido da definição openAPI e armazenado num map<string,Type>
-                val expectedBodyMap = expectedBody.schemaType.fieldsTypes
-                if(requestBodyMap.keys != expectedBodyMap.keys) {
-                    throw IllegalArgumentException("Invalid body - keys")
-                }
-
-                //Converte o body recebido para um map<string,Type>
-                val requestBodyTypes = requestBodyMap.mapValues { convertToType(it.value) }
-
-                //Verifica se os tipos do body recebido correspondem aos tipos esperados
-                requestBodyTypes.forEach { (key, type) ->
-                    if(type != expectedBodyMap[key]!!) {
-                        throw IllegalArgumentException("Invalid body - types")
-                    }
-                }
-
-                return true
-
-                //Se o body não for um json a função convertJsonToMap lança uma exceção
-            } catch (e: Exception) {
-                println(e.message)
-                throw IllegalArgumentException("Invalid body - json")
+    fun verifyHeaders(
+        headers: Map<String, String>,
+        expectedHeaders: List<ApiParameter>,
+        contentType: String
+    ): VerifyHeadersResult {
+        expectedHeaders.forEach { expectedHeader ->
+            if(expectedHeader.required && !headers.containsKey(expectedHeader.name)) {
+                return failure(VerifyHeadersError.MissingHeader)
+            }
+            val headerValue = headers[expectedHeader.name]
+            if(headerValue == null && expectedHeader.required) {
+                return failure(VerifyHeadersError.MissingHeader)
+            }
+            if(headerValue != null && convertToType(headerValue) != expectedHeader.type) {
+                return failure(VerifyHeadersError.InvalidType)
             }
         }
-        else {
-            throw IllegalArgumentException("Invalid body")
+        if(headers["Content-Type"] != contentType) {
+            return failure(VerifyHeadersError.InvalidContentType)
         }
+        val unexpectedHeaders = headers.keys - expectedHeaders.map { it.name }
+        if(unexpectedHeaders.isNotEmpty()) {
+            return failure(VerifyHeadersError.InvalidHeader)
+        }
+        return success(true)
+    }
 
-        return false
-
+    fun verifyBody(
+        body: String,
+        expectedBody: ApiRequestBody
+    ): VerifyBodyResult {
+        return when(expectedBody.schemaType) {
+            is Type.NullType -> if(body.isEmpty()) success(true) else failure(VerifyBodyError.InvalidBodyTypes)
+            is Type.BooleanType -> if(body == "true" || body == "false") success(true) else failure(VerifyBodyError.InvalidBodyTypes)
+            is Type.IntegerType -> if(body.toIntOrNull() != null) success(true) else failure(VerifyBodyError.InvalidBodyTypes)
+            is Type.NumberType -> if(body.toDoubleOrNull() != null) success(true) else failure(VerifyBodyError.InvalidBodyTypes)
+            is Type.StringType -> if(body.isNotEmpty()) success(true) else failure(VerifyBodyError.InvalidBodyTypes)
+            is Type.ArrayType -> {
+                try {
+                    val array = convertStringToArray(body)
+                    val elementType = expectedBody.schemaType.elementsType
+                    array.forEach { element ->
+                        if(elementType != convertToType(element)) {
+                            return failure(VerifyBodyError.InvalidBodyTypes)
+                        }
+                    }
+                    success(true)
+                } catch (e: Exception) {
+                    return failure(VerifyBodyError.InvalidBodyFormat)
+                }
+            }
+            is Type.ObjectType -> {
+                try {
+                    //Converte o body recebido para um map<string,any>
+                    val requestBodyMap = convertJsonToMap(body)
+                    //Body experado extraido da definição openAPI e armazenado num map<string,Type>
+                    val expectedBodyMap = expectedBody.schemaType.fieldsTypes
+                    if(requestBodyMap.keys != expectedBodyMap.keys) {
+                        return failure(VerifyBodyError.InvalidBodyKeys)
+                    }
+                    //Converte o body recebido para um map<string,Type>
+                    val requestBodyTypes = requestBodyMap.mapValues { convertToType(it.value) }
+                    //Verifica se os tipos do body recebido correspondem aos tipos esperados
+                    requestBodyTypes.forEach { (key, type) ->
+                        if(type != expectedBodyMap[key]!!) {
+                            return failure(VerifyBodyError.InvalidBodyTypes)
+                        }
+                    }
+                    success(true)
+                    //Se o body não for um json a função convertJsonToMap lança uma exceção
+                } catch (e: Exception) {
+                    return failure(VerifyBodyError.InvalidBodyFormat)
+                }
+            }
+            Type.UnknownType -> success(true)
+        }
     }
 
     fun convertToType(value: Any?): Type {
@@ -124,6 +191,4 @@ class BodyAndParamsDynamicHandler(
             else -> Type.UnknownType
         }
     }
-
 }
-
--- jvm/src/test/kotlin/isel/openapi/mock/http/DynamicHandlersTests.kt
+++ jvm/src/test/kotlin/isel/openapi/mock/http/DynamicHandlersTests.kt
@@ -1,15 +1,19 @@
 package isel.openapi.mock.http
 
+import isel.openapi.mock.parsingServices.model.ApiParameter
 import isel.openapi.mock.parsingServices.model.ApiRequestBody
+import isel.openapi.mock.parsingServices.model.Location
+import isel.openapi.mock.parsingServices.model.ParameterStyle
 import isel.openapi.mock.parsingServices.model.Type
+import isel.openapi.mock.utils.Failure
+import isel.openapi.mock.utils.Success
 import kotlin.test.Test
-import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class DynamicHandlersTests {
 
     @Test
-    fun verifyBodyTest() {
+    fun objectVerificationTest() {
 
         val expectedBody =
             ApiRequestBody(
@@ -29,65 +33,13 @@ class DynamicHandlersTests {
                 "age": 30
             }
         """.trimIndent()
-
-        val dynamicHandler = BodyAndParamsDynamicHandler(
-            response = "Response",
-            params = null,
-            body = expectedBody
-        )
-
-        assertTrue { dynamicHandler.verifyBody(body, expectedBody) }
-
-    }
-
-    @Test
-    fun verifyBodyTest2() {
-
-        val expectedBody =
-            ApiRequestBody(
-                contentType = "application/json",
-                schemaType = Type.ObjectType(
-                    fieldsTypes = mapOf(
-                        "name" to Type.StringType,
-                        "age" to Type.IntegerType
-                    )
-                ),
-                required = true
-            )
-
-        val body = """
+        val body2 = """
             {
                 "name": "John",
                 "age": "30"
             }
         """.trimIndent()
-
-        val dynamicHandler = BodyAndParamsDynamicHandler(
-            response = "Response",
-            params = null,
-            body = expectedBody
-        )
-
-        assertFailsWith<IllegalArgumentException> { dynamicHandler.verifyBody(body, expectedBody) }
-
-    }
-
-    @Test
-    fun verifyBodyTest3() {
-
-        val expectedBody =
-            ApiRequestBody(
-                contentType = "application/json",
-                schemaType = Type.ObjectType(
-                    fieldsTypes = mapOf(
-                        "name" to Type.StringType,
-                        "age" to Type.IntegerType
-                    )
-                ),
-                required = true
-            )
-
-        val body = """
+        val body3 = """
             bom dia
         """.trimIndent()
 
@@ -97,7 +49,276 @@ class DynamicHandlersTests {
             body = expectedBody
         )
 
-        assertFailsWith<IllegalArgumentException> { dynamicHandler.verifyBody(body, expectedBody) }
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+        assertTrue { dynamicHandler.verifyBody(body3, expectedBody) is Failure }
+
+    }
+
+    @Test
+    fun arrayVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.ArrayType(
+                    elementsType = Type.IntegerType
+                ),
+                required = true
+            )
+
+        val body = """
+            [1,2,3,4]
+        """.trimIndent()
+        val body2 = """
+            [1,2,3,"4"]
+        """.trimIndent()
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+
+    }
+
+    @Test
+    fun objectArrayVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.ArrayType(
+                    elementsType = Type.ObjectType(
+                        fieldsTypes = mapOf(
+                            "name" to Type.StringType,
+                            "age" to Type.IntegerType
+                        )
+                    )
+                ),
+                required = true
+            )
+
+        val body = """
+            [
+                {
+                    "name": "John",
+                    "age": 30
+                },
+                {
+                    "name": "Jane",
+                    "age": 25
+                }
+            ]
+        """.trimIndent()
+        val body2 = """
+            [
+                {
+                    "name": "John",
+                    "age": 30
+                },
+                {
+                    "name": "Jane",
+                    "age": "25"
+                }
+            ]
+        """.trimIndent()
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+
+    }
+
+    @Test
+    fun nullBodyVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.NullType,
+                required = true
+            )
+
+        val body = ""
+        val body2 = "null"
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+    }
+
+    @Test
+    fun booleanBodyVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.BooleanType,
+                required = true
+            )
+
+        val body = "true"
+        val body2 = "false"
+        val body3 = "null"
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body3, expectedBody) is Failure }
+    }
+
+    @Test
+    fun numberBodyVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.NumberType,
+                required = true
+            )
+
+        val body = "3.14"
+        val body2 = "3"
+        val body3 = "null"
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body3, expectedBody) is Failure }
+    }
+
+    @Test
+    fun stringBodyVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.StringType,
+                required = true
+            )
+
+        val body = "Hello"
+        val body2 = ""
+        val body3 = "null"
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+        assertTrue { dynamicHandler.verifyBody(body3, expectedBody) is Success }
+    }
+
+    @Test
+    fun integerBodyVerificationTest() {
+
+        val expectedBody =
+            ApiRequestBody(
+                contentType = "application/json",
+                schemaType = Type.IntegerType,
+                required = true
+            )
+
+        val body = "3"
+        val body2 = "3.14"
+        val body3 = "null"
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = null,
+            body = expectedBody
+        )
+
+        assertTrue { dynamicHandler.verifyBody(body, expectedBody) is Success }
+        assertTrue { dynamicHandler.verifyBody(body2, expectedBody) is Failure }
+        assertTrue { dynamicHandler.verifyBody(body3, expectedBody) is Failure }
+    }
+
+    @Test
+    fun headersVerificationTest() {
+
+        val expectedHeaders = listOf(
+            ApiParameter(
+                name = "Content-Type",
+                location = Location.HEADER,
+                type = Type.StringType,
+                required = true,
+                allowEmptyValue = false,
+                style = ParameterStyle.FORM,
+                explode = false,
+                description = "Content type of the request"
+            ),
+            ApiParameter(
+                name = "Authorization",
+                location = Location.HEADER,
+                type = Type.StringType,
+                required = false,
+                allowEmptyValue = false,
+                style = ParameterStyle.FORM,
+                explode = false,
+                description = "Authorization token"
+            )
+        )
+
+        val headers = mapOf(
+            "Content-Type" to "application/json",
+            "Authorization" to "token123"
+        )
+
+        val headers1 = mapOf(
+            "Content-Type" to "application/json",
+        )
+        val headers2 = mapOf(
+            "Authorization" to "token123"
+        )
+        val headers3 = mapOf(
+            "Content-Type" to "application/problem",
+            "Authorization" to "token123"
+        )
+        val headers4 = mapOf(
+            "Content-Type" to "application/json",
+            "Authorization" to "token123",
+            "Extra" to "extra"
+        )
+
+        val dynamicHandler = BodyAndParamsDynamicHandler(
+            response = "Response",
+            params = expectedHeaders,
+            body = null
+        )
+
+        assertTrue { dynamicHandler.verifyHeaders(headers, expectedHeaders, "application/json") is Success }
+        assertTrue { dynamicHandler.verifyHeaders(headers1, expectedHeaders, "application/json") is Success }
+        assertTrue { dynamicHandler.verifyHeaders(headers2, expectedHeaders, "application/json") is Failure }
+        assertTrue { dynamicHandler.verifyHeaders(headers3, expectedHeaders, "application/json") is Failure }
+        assertTrue { dynamicHandler.verifyHeaders(headers4, expectedHeaders, "application/json") is Failure }
 
     }
 
